b4aa6b453b5c9e24ac0c7bcbaf3c0958743bae0a
William Sørensen <williambilly42@gmail.com>
2023-04-13 23:30:02 +0200

    initial commit

--- build/libraries/Grove_SGP30_Gas_Sensor/sensirion_common.c.d
+++
@@ -1,4 +0,0 @@
-/home/williams/Dev/MicroProjects/static-sensor-and-warning-system/build/libraries/Grove_SGP30_Gas_Sensor/sensirion_common.c.o: \
- /home/williams/Arduino/libraries/Grove_SGP30_Gas_Sensor/sensirion_common.c \
- /home/williams/Arduino/libraries/Grove_SGP30_Gas_Sensor/sensirion_common.h \
- /home/williams/Arduino/libraries/Grove_SGP30_Gas_Sensor/sensirion_configuration.h
