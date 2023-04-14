388c0220af20188971fb3f60d3362c1348c5c8a2
William Sørensen <williambilly42@gmail.com>
2023-04-14 20:44:18 +0200

    fix compiled bins

--- build/libraries/Wire/Wire.cpp.d
+++
@@ -1,8 +0,0 @@
-/home/williams/Dev/MicroProjects/static-sensor-and-warning-system/build/libraries/Wire/Wire.cpp.o: \
- /home/williams/.arduino15/packages/arduino/hardware/avr/1.8.6/libraries/Wire/src/Wire.cpp \
- /home/williams/.arduino15/packages/arduino/hardware/avr/1.8.6/libraries/Wire/src/utility/twi.h \
- /home/williams/.arduino15/packages/arduino/hardware/avr/1.8.6/libraries/Wire/src/Wire.h \
- /home/williams/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Stream.h \
- /home/williams/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Print.h \
- /home/williams/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/WString.h \
- /home/williams/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Printable.h
